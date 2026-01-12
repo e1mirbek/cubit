776d709602d6807b10fed5a4e3db7b83a96985c9
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-12 22:47:52 +0600

    feat(ui): implement UserScreen using BlocBuilder

--- lib/ui/screens/user_screen.dart
+++ lib/ui/screens/user_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cubit_example/controller/users/user_cubit.dart';
+import 'package:cubit_example/controller/users/user_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserScreen extends StatelessWidget {
   final String title;
@@ -6,12 +9,50 @@ class UserScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<UserCubit>();
+
+    // вызываем один раз после build
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      cubit.loadUser();
+    });
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: const Center(child: Text('User Screen')),
+      body: BlocBuilder<UserCubit, UserState>(
+        builder: (context, state) {
+          if (state is UserLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is UserError) {
+            return Center(child: Text('Error: ${state.errorMessage}'));
+          } else if (state is UserLoaded) {
+            return RefreshIndicator(
+              onRefresh: () => cubit.loadUser(),
+              child: ListView.builder(
+                itemCount: state.users.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(color: Colors.grey[200]),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text(state.users[index].id.toString()),
+                      ),
+                      title: Text(state.users[index].name),
+                    ),
+                  );
+                },
+              ),
+            );
+          } else {
+            cubit.loadUser();
+            return const SizedBox.shrink();
+          }
+        },
+      ),
     );
   }
 }
