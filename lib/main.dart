2ec3da86495a82f9f5187abdfb947c44e9283e40
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-14 21:30:50 +0600

    feat(main): switch to MultiBlocProvider and add TaskCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:cubit_example/controller/tasks/task_cubit.dart';
 import 'package:cubit_example/controller/users/user_cubit.dart';
+import 'package:cubit_example/repository/tasks/task_repository.dart';
 import 'package:cubit_example/repository/users/user_repositores.dart';
-import 'package:cubit_example/ui/screens/user_screen.dart';
+import 'package:cubit_example/ui/screens/task_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,12 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => UserCubit(UserRepositoryImpl()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => UserCubit(UserRepositoryImpl())..loadUser(),
+        ),
+        BlocProvider(
+          create: (context) => TaskCubit(TaskRepositoryImpl())..loadTask(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-        home: const UserScreen(title: 'Flutter State Management Demo'),
+        home: const TaskScreen(),
       ),
     );
   }
